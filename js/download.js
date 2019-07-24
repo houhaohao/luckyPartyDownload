dd2c63b97a8ec32c8814e138f83251a74aeb1efa
houhao <houhao@exsample.com>
2019-07-24 13:53:53 +0800

    no message

--- js/download.js
+++ js/download.js
@@ -20,8 +20,8 @@ function downFun() {
         // if (data.code == 200) {
             // ios = iosPrefix + data.data.ios.downloadUrl;
             // ad = data.data.android.downloadUrl;
-            ios ='itms-services://?action=download-manifest&amp;url=https://luckyparty.oss-cn-beijing.aliyuncs.com/public/download/luckyparty/v2.0.3/manifest.plist';
-            ad ='http://luckyparty.ufile.ucloud.com.cn/apk/LuckyParty_V2.0.2.apk';
+            ios ='itms-services://?action=download-manifest&amp;url=https://luckyparty.oss-cn-beijing.aliyuncs.com/public/download/luckyparty/v2.0.4/manifest.plist';
+            ad ='http://luckyparty.ufile.ucloud.com.cn/apk/LuckyParty_V2.0.3.apk';
             $('.js-ios').attr("href", ios)
                 // $('.js-ad').attr("href",ad)
             $('.pcjs-ad').attr("href", ad)
