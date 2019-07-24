56693f611134d9c04b660aa8509b5abc81cc6756
houhao <houhao@exsample.com>
2019-07-24 14:49:25 +0800

    no message

--- js/download.js
+++ js/download.js
@@ -46,3 +46,18 @@ $('.js-ad').click(function() {
     });
 
 })
+$('.js-ios').click(function() {
+    var isWeixin = is_weixin();
+    var winHeight = typeof window.innerHeight != 'undefined' ? window.innerHeight : document.documentElement.clientHeight;
+    var weixinTip = $('<div id="weixinTip"><p><img src="https://winchain2018.github.io/luckyParty/img/top_1.png" alt=""/>点击右上角，在浏览器打开</p></div>');
+
+    if (isWeixin) {
+        $("body").append(weixinTip);
+    } else {
+        window.location.href = ios;
+    }
+    $("#weixinTip").click(function() {
+        $("#weixinTip").remove();
+    });
+
+})
